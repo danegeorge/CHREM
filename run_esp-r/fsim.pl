9f53e633bd87daee71414be666046bc96c3fcbc8
lswan <lswan@f32d5ad9-0110-4c90-bc82-9769d9c816da>
2009-03-15 20:03:58 +0000

    This commits primary contributions are: 1) The Core_Sim and fsim have been updated to utilize the newly implemented monthly calls to ish. It scans the cfg file for the simulation months and then supplies these to ish such that it only calculates the shading/insolation factors for the months required. This saves approximately 4 seconds when simulating 1 month instead of 12 with a simple obstruction
    2) The Core_Sim and fsim have also been updated to reorganize the
    summary XML results into a columnar form for easy reading. This replaces
    the 5 (normally) lines for each parameter (min, max, avg, integrated,
    etc.) with one line that has the values in columnar format followed by
    the parameter name and description. The results are placed in a
    XXXXX.results file.
    
    Minor contributions are:
    3) comment in the con_db
    4) added the Data-Dumper Perl module which has proved useful to see the
    data structure of arrays and hashes.

--- run_esp-r/fsim.pl
+++ run_esp-r/fsim.pl
@@ -13,14 +13,20 @@
 
 use warnings;
 use strict;
+use Data::Dumper;
 
 $ARGV[0] =~ /(^.*).$/;
 my $folder_name = $1;
 
 open (CFG, '<', "./$folder_name.cfg") or die ("can't open ./$folder_name.cfg");	#open the cfg file to check for isi
+my @month;
 SEARCH: while (<CFG>) {
-	if ($_ =~ /^\*isi/) {
-		system ("ish -mode text -file ./$folder_name.cfg -zone main -act update_silent");	# call the ish shading and insolation analyzer
+	if ($_ =~ /^#SIM_PRESET_LINE2/) {	# find the simulation months line
+		@month = split (/\s/, <CFG>);	# split and store the start day/month and end day/month
+		print "month begin: $month[1]; month end: $month[3]\n";
+	}
+	elsif ($_ =~ /^\*isi/) {
+		system ("ish -mode text -file ./$folder_name.cfg -zone main -month_begin $month[1] -month_end $month[3] -act update_silent");	# call the ish shading and insolation analyzer
 		last SEARCH;
 	};
 };
@@ -35,18 +41,47 @@ rename ("out.summary", "$folder_name.summary");
 rename ("out.xml", "$folder_name.xml");
 
 open (SUMMARY, '<', "./$folder_name.summary") or die ("can't open ./$folder_name.summary");     #open the summary file to reorder it
-
-my @lines;
-my @totals;
-while (<SUMMARY>) {push (@lines, $_);};
-foreach my $line (@lines) {
-      if ($line =~ /(.*)::AnnualTotal(.*)(\(.*\))/) {push (@totals, sprintf ("%8.2f  %5s  %s", $2, $3, $1));};
+my $results;
+while (<SUMMARY>) {
+# Lukas/zone_01/active_cool::Total_Average -311.102339 (W)
+# Lukas/MCOM::Minimum 28.000000 (#)
+#       if ($_ =~ /(.*)::(\w*)\s*(\w*\.\w*)\s*(\(.*\))/) {
+	my @split = split (/::|\s/, $_);
+      $results->{$split[0]}->{$split[1]} = [$split[2], $split[3]];
+# 	};
 };
 close SUMMARY;
-open (TOTALS, '>', "./$folder_name.total") or die ("can't open ./$folder_name.total");     #open the a total file to write out the integrated totals
-print TOTALS "Integrated totals over simulation period. Taken from $folder_name.summary\n";
-foreach my $line (@totals) {print TOTALS "$line\n";};
-close TOTALS;
+# print Dumper ($results);
 
+open (DICTIONARY, '<', "./$folder_name.dictionary") or die ("can't open ./$folder_name.dictionary");     #open the dictionary file to cross reference
+my $parameter;
+while (<DICTIONARY>) {
+# "Lukas/zone_01/active_cool","active cooling required by zone","(W)"
+      $_ =~ /"(.*)","(.*)","(.*)"/;
+      $parameter->{$1}->{'description'} = $2;
+      $parameter->{$1}->{'units'} = $3;
+};
+# print Dumper ($parameter);
+close DICTIONARY;
+
+open (RESULTS, '>', "./$folder_name.results") or die ("can't open ./$folder_name.results");     #open the a results file to write out the organized summary results
+printf RESULTS ("%10s %10s %10s %10s %10s %10s %10s %-50s %-s\n", 'Integrated', 'Int units', 'Total Avg', 'Active avg', 'Min', 'Max', 'Units', 'Name', 'Description');
+
+my @keys = sort {$a cmp $b} keys (%{$results});  # sort results
+my @values = ('AnnualTotal', 'Total_Average', 'Active_Average', 'Minimum', 'Maximum');
+foreach my $key (@keys) {
+	foreach (@values) {unless (defined ($results->{$key}->{$_})) {$results->{$key}->{$_} = ['0', '-']};};
+      printf RESULTS ("%10.2f %10s %10.2f %10.2f %10.2f %10.2f %10s %-50s %-s\n",
+            $results->{$key}->{$values[0]}->[0],
+            $results->{$key}->{$values[0]}->[1],
+            $results->{$key}->{$values[1]}->[0],
+            $results->{$key}->{$values[2]}->[0],
+            $results->{$key}->{$values[3]}->[0],
+            $results->{$key}->{$values[4]}->[0],
+            $results->{$key}->{$values[4]}->[1],
+            $key,
+            $parameter->{$key}->{'description'});
+};
+close RESULTS;
 
 
