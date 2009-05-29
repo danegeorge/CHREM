4a457a2f429e22f1004e135f5b5809059c77c475
lswan <lswan@f32d5ad9-0110-4c90-bc82-9769d9c816da>
2009-05-29 14:25:51 +0000

    This commit significantly extends the NN_Gen script.
    The xml is read in and acted upon. For each type of value, it is normalized and modification is made to account for presence.
    The house listing is then read in, and using information on the number of houses for each house_type-region, the disributions are inflated.
    Using the inflated distributions, arrays are indexed out with corresponding header values.
    These arrays are shuffled for randomness.
    
    Finally, the data is output into the form required as an input for the NN.
    The NN was tested (script was also updated) and approporiate values were carried over for use in the CHREM.
    
    
    
    Former-commit-id: 51cd95cad49d5a884755b15066f4d37dc51f399b

--- scripts/NN_Gen.pl
+++ scripts/NN_Gen.pl
@@ -37,6 +37,7 @@ use threads;	# threads-1.71 (to multithread the program)
 # use File::Copy;	# (to copy the input.xml file)
 use XML::Simple;	# to parse the XML databases for esp-r and for Hse_Gen
 use Data::Dumper;
+use List::Util 'shuffle';
 
 # --------------------------------------------------------------------
 # Declare the global variables
@@ -84,25 +85,120 @@ COMMAND_LINE: {
 	
 };
 
+# declare an array to hold all of the hse_type and region varieties (e.g. SD, AT, SD-AT) as well as the presence, header, and ALL
+# this will be used in the ForceArray command so that the logic works even if there is only one element (that would otherwise default to a hash)
+my @distribution_options = ('node', 'header', 'ALL');
+# declare an array to hold the type_regions so as to fill out for each combination (e.g. SD-AT)
+my @types_regions;
 
-my @distribution_options = ('header');	# declare an array to hold all of the hse_type and region varieties (e.g. SD, AT, SD-AT
-
-
-# Cycle through each of the types to generate a list
-foreach my $hse_type (values (%hse_names_only)) {
-	push (@distribution_options, $hse_type);	
-	foreach my $region_name (values (%region_names_only)) {
+# Cycle through each of the house type and region varieties to generate the list
+foreach my $hse_type (values (%hse_names_only)) {	# house types
+	push (@distribution_options, $hse_type);	# remember the house type by itself
+	
+	foreach my $region_name (values (%region_names_only)) {	# regions
+		# remember the region name and the combination of the house type and region name
 		push (@distribution_options, $region_name, "$hse_type-$region_name");
+		push (@types_regions, "$hse_type-$region_name");
 	};
 };
 
-print "@distribution_options\n";
+# print "@distribution_options\n";
 
-# Readin the ALC and DHW xml files and force arrays for the distribution_options
+# Readin the ALC and DHW xml files and force certain arrays for the distribution_options
 my $ALC_xml = XMLin("../NN/NN_model/ALC_distributions.xml", ForceArray => [@distribution_options]);	# readin the ALC xml
 # my $DHW_xml = XMLin("../NN/NN_model/DHW_distributions.xml", ForceArray => [@distribution_options]);	# readin the DHW xml
 
+# print Dumper $ALC_xml;
+
+# Cycle through the nodes to list them in the header
+# AND to check the xml data for validity
+
+foreach my $node (@{$ALC_xml->{'node'}}) {
+
+	# Check the xml data for validity
+	foreach my $value (@{$node->{'header'}}) {	# check each value of the header (which is the value information)
+		# compare it to the min and max values and die if out of range
+		if ($value < $node->{'min'} || $value > $node->{'max'}) {
+			die ("XML Source Issue @ Node: $node->{'var_name'}. Value = $value; min = $node->{'min'}; max = $node->{'max'}\n");
+		};
+	};
+	
+	# normalize each data element by the sum of the row and then make an allowance for the presence factor
+	foreach my $data_type (keys (%{$node->{'presence'}})) {	# use the presence as the key to finding data rows
+		my $sum = 0;	# initialize a summation
+		foreach my $element (@{$node->{$data_type}}) {$sum = $sum + $element};	# sum the elements and store
+		# normalize the elements by the sum and then multiply by the presence factor
+		foreach my $element (@{$node->{$data_type}}) {$element = $element / $sum * $node->{'presence'}->{$data_type}};
+		
+		# Check to see if the presence factor is less than one which would indicate we need to supply a minimum term 
+		if ($node->{'presence'}->{$data_type} < 1) {
+		
+			# this checks to see if the minimum value already exists. If it does, then it is added to. If it does not, then a location is created.
+			CHECK_FOR_ZERO: {
+				# go through the header as that is where the minimum value would be
+				foreach my $element (0..$#{$node->{'header'}}) {
+				
+					# check to see that the header includes the minimum value. If it does then add to the correct value of the data array. Not the use of the && which is because we need to cycle through this loop for each data type. If the array sizes are different, it means that the header DID NOT initially include the value, it was simply set by a previous data loop.
+					if ($node->{'header'}->[$element] == $node->{'min'} && @{$node->{$data_type}} == @{$node->{'header'}}) {
+						# then increase it by the difference between 1 and the presence
+						
+						$node->{$data_type}->[$element] = $node->{$data_type}->[$element] + (1 - $node->{'presence'}->{$data_type});
+						last CHECK_FOR_ZERO;	# jump out of loop because the correct location was found
+					};
+				};
+				
+				# we did not find the minimum value in the header, so create this location and populate it with the difference between 1 and the presence
+				push (@{$node->{$data_type}}, 1 - $node->{'presence'}->{$data_type});
+				# only push the minimum value onto the header if the arrays are different sizes. This is again to deal with the multiple loop passes over all of the data types.
+				if (@{$node->{$data_type}} != @{$node->{'header'}}) {
+					push (@{$node->{'header'}}, $node->{'min'});
+				};
+			};
+		};
+	};
+	
+	# go through each type_region and check for definition. If it is not defined, then creat it with the most suitable next up resolution level of data
+	foreach my $type_region (@types_regions) {
+		# split up the name and store the hse_type and region for later use
+		$type_region =~ /^(..)-(..)$/ or die ("\nMalformed type_region array: $type_region\n");
+		my $hse_type = $1;
+		my $region = $2;
+		
+		my $res;	# resolution level. This will be filled with the most relevant data type name (e.g. in preferred order: SD-AT, SD, AT, ALL)
+		
+		if (defined ($node->{$type_region})) {$res = $type_region;}	# Fine resolution and nothing is required
+		elsif (defined ($node->{$hse_type})) {$res = $hse_type;}	# house type resolution is best we have
+		elsif (defined ($node->{$region})) {$res = $region;}	# regional resolution is the best we have
+		elsif (defined ($node->{'ALL'})) {$res = 'ALL';}	# national resolution is all we have
+		else {die ("\nCannot find distribution information for node $node->{'var_name'}; checked $type_region, $hse_type, $region, and 'ALL'\n");};
+		
+		# if the resolution is not equal to the type and region then we have to use a higher resolution distribution and set that for the type_region
+		unless ($res eq $type_region) {
+			# cycle through all of the data for the closest resolution level
+			foreach my $element (@{$node->{$res}}) {
+				# set the type_region element equal to that of the closest resolution element
+				push (@{$node->{$type_region}}, $element);
+			};
+		};
+	};
+};
+
 print Dumper $ALC_xml;
+
+# ----------------------------------------------------------------------------------------------------------------------
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ----------------------------------------------------------------------------------------------------------------------
+# AT THIS POINT, THE XML DATA HAS BEEN READ IN AND THE DISTRIBUTIONS HAVE BEEN OPERATED ON IN SUCH A FASHION AS TO ACCOUNT FOR THE DISTRIBUTION AND PRESENCE.
+
+# THE DATA IS ENTIRELY CONTAINED IN A HASH REFERENCE WHERE THE KEY IS AN ARRAY FOUND AT THE REFERENCE:
+# $ALC_xml->{'node'}->[array of nodes]->{'header'}
+
+# AND THE ACTUAL DATA IS (for each type-region; e.g. SD-AT):
+# $ALC_xml->{'node'}->[array of nodes]->{'SD-AT'}->[array of distribution data totalling a value of 1.]
+# ----------------------------------------------------------------------------------------------------------------------
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ----------------------------------------------------------------------------------------------------------------------
+
 
 # -----------------------------------------------
 # Read in the DHW and AL annual energy consumption CSDDRD listing
@@ -112,13 +208,23 @@ open (DHW_AL, '<', "../CSDDRD/CSDDRD_DHW_AL_annual.csv") or die ("can't open dat
 open (ALC, '>', "../NN/NN_model/ALC-Inputs-V2.csv") or die ("can't open datafile: ../NN/NN_model/ALC-Inputs-V2.csv");
 # open (DHW, '>', "../NN/NN_model/DHW-Inputs-V2.csv") or die ("can't open datafile: ../NN/NN_model/DHW-Inputs-V2.csv");
 
+# print the first two fields of the NN Input information
+# print ALC "*header,File_name,House_type,Region";
 print ALC "Number,File_name";
-
+# print DHW "Number,File_name";
 foreach my $node (@{$ALC_xml->{'node'}}) {
-	print ALC ",$node->{'var_name'}";
+	print ALC ",$node->{'var_name'}";	# print the node name
 };
+print ALC "\n";	# newline b/c we have reached the end of the NN Input header
 
-print ALC "\n";
+# # print additional information
+# foreach my $tag ('units', 'min', 'max')
+# 	print ALC "*$tag,-,-,-";
+# 	foreach my $node (@{$ALC_xml->{'node'}}) {
+# 		print ALC ",$node->{$tag}";	# print the node name
+# 	};
+# 	print ALC "\n";	# newline b/c we have reached the end of the NN Input header
+# };
 
 my $dhw_al;	# declare a 2D array that is a hash ref (first array is hse_type, second is region, then hash ref at hse file name)
 my @dhw_al_header;	# store the header info
@@ -133,11 +239,53 @@ while (<DHW_AL>) {	# cycle through the remainder of the file
 };
 close DHW_AL;
 
-# foreach
-# 
-# foreach my $hse_type (@{$dhw_al}) {
-# 	foreach my $type_region (@{$hse_type}) {
-# 		my $count = @{$type_region};
-# 		
 
+# GO THROUGH THE HOUSE TYPES AND REGIONS SO AS TO BUILD ARRAYS WITH THE RANDOMIZED VALUES FOR APPLICATION TO THE HOUSES
+# go through each house type
+foreach my $type_num (1..$#{$dhw_al}) {
+	# go through each region
+	foreach my $region_num (1..$#{$dhw_al->[$type_num]}) {
+	
+		# count the number of houses
+		my $count = @{$dhw_al->[$type_num]->[$region_num]};
+		print "Type: $type_num; Region: $region_num; Count: $count\n";
+		
+		# discern the names of the type and region using the hashes
+		my $type_name = $hse_names_only{$type_num};
+		my $region_name = $region_names_only{$region_num};
+		
+		my $data;	# declare an array reference to store all of the developed data structures that hold the input data to the NN
+		
+		# go through each distribution node
+		foreach my $node (@{$ALC_xml->{'node'}}) {
+
+			push (@{$data}, []);	# add an array reference to the data array reference
+
+			# go through each element of the header, remember this is the value to be provided to the house file
+			foreach my $element (0..$#{$node->{'header'}}) {
+
+				# determine how long to make the array from the value in the data line that corresponds to the header value
+				my $array_space = sprintf("%.f", $node->{"$type_name-$region_name"}->[$element] * $count) + @{$data->[$#{$data}]};
+				
+				# go through the spacing and set the array equal to the header value. This will generate a large array with ordered values corresponding to the distribution and the header
+				foreach my $position (@{$data->[$#{$data}]}..$array_space) {
+					$data->[$#{$data}]->[$position] = $node->{'header'}->[$element];
+				};
+			};
+			
+			# shuffle the array to get randomness
+			@{$data->[$#{$data}]} = shuffle (@{$data->[$#{$data}]});
+			print "@{$data->[$#{$data}]}\n";
+		};
+		
+		foreach my $house (0..$#{$dhw_al->[$type_num]->[$region_num]}) {
+# 			printf ALC ("%u,%s,%u,%u", $house + 1, $dhw_al->[$type_num]->[$region_num]->[$house], $type_num, $region_num);
+			printf ALC ("%u,%s", $house + 1, $dhw_al->[$type_num]->[$region_num]->[$house]);
+			foreach my $field (@{$data}) {
+				print ALC ",$field->[$house]";
+			};
+			print ALC "\n";
+		};
+	};
+};
 
