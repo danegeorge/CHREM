6fd2c20cebed26f3cb266acc7a3b7e906efce8b4
lswan <lswan@f32d5ad9-0110-4c90-bc82-9769d9c816da>
2009-07-03 23:00:36 +0000

    This commit introduces the concept of localized perl modules to my work!
    Until now, code has not been modularized. Although I was calling other scripts from scripts, it was not easy and was 
    accomplished via system calls which don't facilitate sharing of data in memory.
    
    This is now all changed. I have learned how to use Perl Modules which are simply called in a script via a 'use' statement, 
    giving access to all of the associated subroutines within the module file. Data can be passed back and forth exactly the same 
    as a local subroutine via the use of the exporter.
    
    There is now a sub-directory in the scripts directory called CHREM_modules.
    I hav put in two new modules (denoted by the *.pm extention). They are:
    
    1) Database.pm, which simply moves out the database code (XML to ESP-r style and also data storage) to this perl module. This 
    provided clarity.
    2) Cross_ref.pm which now contains two subroutines. One reads in CSV files that are tagged (e.g. climate, DHW & AL, BCD, 
    etc.) and the other reads in XML files with some user output and consideration given to ForceArray.
    
    The new use of these two modules was implemented in the Hse_Gen and NN_Input_Gen scripts. It provides one subroutine that is 
    now called many times to load data, thereby saving significant debugging, etc. It also provides commonality and code reuse.
    
    In the future items like min/max and other functions will be migrated from the individual scripts to the modules.
    
    
    Basic use: 
    The module template may be seen by looking at either module. It simply has a name and uses the exporter to make its 
    subroutines local to the calling script.
    The subroutines are then listed normally with return function so that data is passed back to the calling routine.
    
    Within a script the module is brought in by 
    use CHREM_modules::Module.pm ('subroutine', 'list')
    Then the subroutine is just called as a bareword.

---
+++ scripts/CHREM_modules/Cross_ref.pm
@@ -0,0 +1,99 @@
+# ====================================================================
+# CHREM_modules::Cross_ref.pl
+# Author: Lukas Swan
+# Date: July 2009
+# Copyright: Dalhousie University
+# ====================================================================
+# The following subroutines are included in the perl module:
+# cross_ref_readin: a subroutine that reads in information from a tagged file and stores it in a hash reference
+# key_XML_readin: a subroutine that reads in XML information with specific ForceArray information
+# ====================================================================
+
+# Declare the package name of this perl module
+package CHREM_modules::Cross_ref;
+
+# Declare packages used by this perl module
+use strict;
+use CSV;	# CSV-2 (for CSV split and join, this works best)
+use XML::Simple;	# to parse the XML databases
+
+# Set the package up to export the subroutines for local use within the calling perl script
+require Exporter;
+our @ISA = ('Exporter');
+our @EXPORT_OK = ('cross_ref_readin', 'key_XML_readin');
+
+# ====================================================================
+# cross_ref_readin
+# This routines reads in cross referencing file that are tagged with 
+# *header and *data information. It stores the information as hash references
+# using the first column after the tag as the key. It then returns this 
+# reference to the calling script.
+# ====================================================================
+
+sub cross_ref_readin {
+	# shift the passed file path
+	my $file = shift();
+	# show the user that we are working on this file. Note that the system call is used due to timing which can lead to a print statment not showing up until the readin is complete
+	system ("printf \"Reading $file\"");
+
+	# Open and read the crosslisting, note that the file handle below is a variable so that it simply goes out of scope
+	open (my $FILE, '<', $file) or die ("can't open datafile: $file");
+
+	my $cross_ref;	# create an crosslisting hash reference
+
+	while (<$FILE>) {
+		$_ =~ s/\r\n$|\n$|\r$//g;	# chomp the end of line characters off (dos, unix, or mac)
+		$_ =~ s/^\s+|\s+$//g;	# remove leading and trailing whitespace
+		
+		if ($_ =~ s/^\*header,\w+,//) {	# header row has *header tag, so remove this portion, and the key (first header value) leaving the CSV information
+			$cross_ref->{'header'} = [CSVsplit($_)];	# split the header into an array
+		}
+			
+		elsif ($_ =~ s/^\*data,//) {	# data lines will begin with the *data tag, so remove this portion, leaving the CSV information
+			@_ = CSVsplit($_);	# split the data onto the @_ array
+			my $key = shift();	# shift off the first element and use it as the key to the hash
+			
+			# create a hash slice that uses the header and data array
+			# although this is a complex structure it simply creates a hash with an array of keys and array of values
+			# @{$hash_ref}{@keys} = @values
+			@{$cross_ref->{'data'}->{$key}}{@{$cross_ref->{'header'}}} = @_;
+		};
+	};
+	
+	# notify the user we are complete and start a new line
+	print " - Complete\n";
+	
+	# Return the hash reference that includes all of the header and data
+	return ($cross_ref);
+};
+
+
+# ====================================================================
+# key_XML_readin
+# This subroutine recieves the path to an XML file and a reference to 
+# an array of labels that should be forced into an array. It then prints
+# some user info and reads in all of the XML info with attention paid to
+# the ForceArray
+# ====================================================================
+
+sub key_XML_readin {
+	# shift the passed file path
+	my $file = shift();
+	# shift the reference to the array that holds the ForceArray labels
+	my $ForceArray = shift();
+	
+	# show the user that we are working on this file. Note that the system call is used due to timing which can lead to a print statment not showing up until the readin is complete
+	system ("printf \"Reading $file\"");
+	
+	my $cross_ref = XMLin($file, ForceArray => $ForceArray);	# readin the xml information with the appropriate force array (note that ForceArray requires an array reference which is provided by the shift above. If this later become longhand array style then you have to provide a unnamed referece i.e. ForceArray => [@array])
+
+	# notify the user we are complete and start a new line
+	print " - Complete\n";
+	
+	# Return the hash reference that includes all of the header and data
+	return ($cross_ref);
+};
+
+
+# Final return value of one to indicate that the perl module is successful
+1;
---
+++ scripts/CHREM_modules/Database.pm
@@ -0,0 +1,304 @@
+# ====================================================================
+# CHREM_modules::Database.pl
+# Author: Lukas Swan
+# Date: July 2009
+# Copyright: Dalhousie University
+# ====================================================================
+# The following subroutines are included in the perl module:
+# database_XML: this file reads the XML files and generates ESP-r database and passes the info back to the calling program
+# ====================================================================
+
+# Declare the package name of this perl module
+package CHREM_modules::Database;
+
+# Declare packages used by this perl module
+use strict;
+use XML::Simple;	# to parse the XML databases
+
+# Set the package up to export the subroutines for local use within the calling perl script
+require Exporter;
+our @ISA = ('Exporter');
+our @EXPORT_OK = ('database_XML');
+
+# ====================================================================
+# database_XML
+# This subroutine generates the esp-r database files to facilitate opening 
+# CSDDRD houses within prj. This script reads the material and 
+# composite construction XML databases and generates the appropriate 
+# ASCII columnar delimited format files required by ESP-r. It then passes
+# back the XML information in hash format to the calling program for use
+# ====================================================================
+
+sub database_XML() {
+	my $mat_name;	# declare an array ref to store (at index = material number) a reference to that material in the mat_db.xml
+	my $con_name;	# declare a hash ref to store (at key = construction name) a reference to that construction in the con_db.xml
+	my $optic_data;	# declare a hash ref to store the optical data from optic_db.xml
+
+	my $mat_data;	# declare repository for mat_db.xml readin
+	my $con_data;	# declare repository for con_db.xml readin
+# 		my $optic_data;	# declare repository for optic_db.xml readin
+
+	MATERIALS: {
+		$mat_data = XMLin("../databases/mat_db.xml", ForceArray => 1);	# readin the XML data, note that any hash with properties will recieve an array index even if there is only one of that hash
+		open (MAT_DB_XML, '>', "../databases/mat_db_regen.xml") or die ("can't open  ../databases/mat_db_regen.xml");	# open a writeout file
+		print MAT_DB_XML XMLout($mat_data);	# printout the XML data
+		close MAT_DB_XML;
+
+		NEW_FORMAT: {	# the tagged format version 1.1
+			open (MAT_DB, '>', "../databases/mat_db_xml_1.1.a") or die ("can't open  ../databases/mat_db_xml_1.1.a");	# open a writeout file
+			open (MAT_LIST, '>', "../databases/mat_db_xml_list") or die ("can't open  ../databases/mat_db_xml_list");	# open a list file that will simply list the materials for use as a reference when making composite constructions
+
+			print MAT_DB "*Materials 1.1\n";	# print the head tag line
+			my $time = localtime();	# determine the time
+			printf MAT_DB ("%s,%s\n", "*date", $time);	# print the time
+			print MAT_DB "*doc,Materials database (tagged format) constructed from mat_db.xml by DB_Gen.pl\n#\n";	# print the documentation tag line
+
+			printf MAT_DB ("%d%s", $#{$mat_data->{'class'}}," # total number of classes\n#\n");	# print the number of classes
+
+			# specification of file format
+			printf MAT_DB ("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
+				"# Material classes are listed as follows:",
+				"#	*class, 'class number'(2 digits),'number of materials in class','class name'",
+				"#	'class description",
+				"#",
+				"# Materials within each class are listed as follows:",
+				"#	*item,'material name','material number'(20 * 'class number' + 'material position within class'; 3 digits),'class number'(2 digits),'material description'",
+				"# The material tag is followed by the following material attributes:",
+				"#	conductivity (W/(m-K), density (kg/m**3), specific heat (J/(kg-K),",
+				"#	emissivity out (-), emissivity in (-), absorptivity out, (-) absorptivity in (-),",
+				"#	diffusion resistance (?), default thickness (mm),",
+				"#	flag [-] legacy [o] opaque [t] transparent [g] gas data+T cor [h] gas data at 4T",
+				"#",
+				"#	transparent material include additional attributes:",
+				"#		longwave tran (-), solar direct tran (-), solar reflec out (-), solar refled in (-),",
+				"#		visable tran (-), visable reflec out (-), visable reflec in (-), colour rendering (-)"
+			);
+
+			print MAT_LIST "Materials database constructed from material_db.xml by DB_Gen.pl\n\n";
+
+			foreach my $class_num (0..$#{$mat_data->{'class'}}) {	# iterate over each class
+				my $class = $mat_data->{'class'}->[$class_num];	# simplify the class reference to a simple scalar for use
+
+				if ($class->{'class_name'} eq 'gap') {$class->{'class_name'} = 'Gap';};
+				print MAT_LIST "\n$class->{'class_name'} : $class->{'description'}\n";	# print the class name and description to the list
+
+				unless ($class->{'class_name'} eq 'Gap') {	# do not print out for Gap
+					print MAT_DB "#\n#\n# CLASS\n";	# print a common identifier
+
+					printf MAT_DB ("%s,%2d,%2d,%s\n",	# print the class information
+						"*class",	# class tag
+						$class_num,	# class number
+						$#{$class->{'material'}} + 1,	# number of materials in the class
+						"$class->{'class_name'}"	# class name
+					);
+					print MAT_DB "$class->{'description'}\n";	# print the class description
+
+					print MAT_DB "#\n# MATERIALS\n";	# print a common identifier
+				};
+
+				foreach my $mat_num (0..$#{$class->{'material'}}) {	# iterate over each material within the class
+					my $mat = $class->{'material'}->[$mat_num];
+					$mat_name->{$mat->{'mat_name'}} = $mat;	# set mat_name equal to a reference to the material
+					if ($class->{'class_name'} eq 'Gap') {$mat->{'mat_num'} = 0;}	# material is Gap so set equal to mat_num 0
+					else {$mat->{'mat_num'} = ($class_num - 1) * 20 + $mat_num + 1;};	# add a key in the material equal to the ESP-r material number
+
+					print MAT_LIST "\t$mat->{'mat_name'} : $mat->{'description'}\n";	# material name and description
+
+					unless ($class->{'class_name'} eq 'Gap') {	# do not print out for Gap
+						printf MAT_DB ("%s,%s,%3d,%2d,%s",	# print the material title line
+							"*item",	# material tag
+							"$mat->{'mat_name'}",	# material name
+							$mat->{'mat_num'},	# material number (groups of 20)
+							$class_num,
+							"$mat->{'description'}\n"	# material description
+						);
+
+						# print the first part of the material data line
+						foreach my $property ('conductivity_W_mK', 'density_kg_m3', 'spec_heat_J_kgK', 'emissivity_out', 'emissivity_in', 'absorptivity_out', 'absorptivity_in', 'vapor_resist') {
+							printf MAT_DB ("%.3f,", $mat->{$property});
+						};
+						printf MAT_DB ("%.1f", $mat->{'default_thickness_mm'});	# this property has a different format but is on the same line
+
+						if ($mat->{'type'} eq "OPAQ") {print MAT_DB ",o\n";} # opaque material so print last digit of line
+						elsif ($mat->{'type'} eq "TRAN") {	# translucent material so print t and additional data
+							print MAT_DB ",t,";	# print TRAN identifier
+							# print the translucent properties
+							foreach my $property ('trans_long', 'trans_solar', 'trans_vis', 'refl_solar_out', 'refl_solar_in', 'refl_vis_out', 'refl_vis_in') {
+								printf MAT_DB ("%.3f,", $mat->{'optic_mat_props'}->[0]->{$property});
+							};
+							printf MAT_DB ("%.3f\n", $mat->{'optic_mat_props'}->[0]->{'clr_render'});	# print the last part of translucent properties line
+						};
+					};
+				};
+			};
+			print MAT_DB "*end\n";	# print the end tag
+			close MAT_DB;
+			close MAT_LIST;
+		};
+	};
+
+
+	OPTICS: {
+		$optic_data = XMLin("../databases/optic_db.xml", ForceArray => 1);	# readin the XML data, note that any hash with properties will recieve an array index even if there is only one of that hash
+		open (OPTIC_DB_XML, '>', "../databases/optics_db_regen.xml") or die ("can't open  ../databases/optics_db_regen.xml");	# open a writeout file
+		print OPTIC_DB_XML XMLout($optic_data);	# printout the XML data
+		close OPTIC_DB_XML;
+
+		open (OPTIC_DB, '>', "../databases/optic_db_xml.a") or die ("can't open  ../databases/optic_db_xml.a");	# open a writeout file for the optics database
+		open (OPTIC_LIST, '>', "../databases/optic_db_xml_list") or die ("can't open  ../databases/optic_db_xml_list");	# open a list file that will simply list the optic name and description 
+
+		# provide the header lines and instructions to the optics database
+		print OPTIC_DB "# optics database (columnar format) constructed from con_db.xml by DB_Gen.pl based on mat_db.xml\n#\n";
+
+		# print the file format
+		foreach my $statement (
+			"# optical properties db for default windows and most of the information",
+			"# required to automatically build transparent constructions & tmc files.",
+			"#",
+			"# 1st line of each item is column sensitive and holds:",
+			"# an identifier (12 char) followed by a description",
+			"# 2nd line holds:",
+			"# a) the number of default (always 1?) and tmc layers (equal to construction)",
+			"# b) visable trans ",
+			"# c) solar reflectance (outside)",
+			"# d) overall solar absorbed",
+			"# e) U value (for reporting purposes only)",
+			"# 3rd line holds:",
+			"# a) direct solar tran at 0deg 40deg 55deg 70deg 80deg from normal",
+			"# b) total heat gain at the same angles (for reporting purposes only)",
+			"# then for each layer there is a line containing",
+			"# a) refractive index",
+			"# b) solar absorption at 0deg 40deg 55deg 70deg 80deg from normal",
+			"#",
+			"#"
+			) {printf OPTIC_DB ("%s\n", $statement);
+			};
+
+		my @optics = sort {$a cmp $b} keys (%{$optic_data});	# sort optic types to order the printout
+
+
+		foreach my $optic (@optics) {
+
+			my $opt = $optic_data->{$optic}->[0];	# shorten the name for subsequent use
+
+			# fill out the optics database (TMC)
+			printf OPTIC_DB ("%-14s%s\n",
+				$optic,	# print the optics name
+				": $opt->{'description'}"	# print the optics description
+			);
+
+			printf OPTIC_LIST ("%-14s%s\n",
+				$optic,	# print the optics name
+				": $opt->{'description'}"	# print the optics description
+			);
+
+			print OPTIC_DB "# $opt->{'optic_con_props'}->[0]->{'optical_description'}\n";	# print additional optical description
+
+			# print the one time optical information
+			printf OPTIC_DB ("%s%4d%7.3f%7.3f%7.3f%7.3f\n",
+				"  1",
+				$#{$opt->{'layer'}} + 1,
+				$opt->{'optic_con_props'}->[0]->{'trans_vis'},
+				$opt->{'optic_con_props'}->[0]->{'refl_solar_doc_only'},
+				$opt->{'optic_con_props'}->[0]->{'abs_solar_doc_only'},
+				$opt->{'optic_con_props'}->[0]->{'U_val_W_m2K_doc_only'}
+			);
+
+			# print the transmission and heat gain values at different angles for the construction type
+			printf OPTIC_DB ("  %s %s\n",
+				$opt->{'optic_con_props'}->[0]->{'trans_solar'},
+				$opt->{'optic_con_props'}->[0]->{'heat_gain_doc_only'}
+			);
+
+			print OPTIC_DB "# layers\n";	# print a common identifier
+			# print the refractive index and abs values at different angles for each layer of the transluscent construction type
+			foreach my $layer (@{$opt->{'layer'}}) {	# iterate over construction layers
+				printf OPTIC_DB ("  %4.3f %s\n",
+					$layer->{'refr_index'},
+					$layer->{'absorption'}
+				);
+			};
+		};
+
+		close OPTIC_DB;
+		close OPTIC_LIST;
+	};
+
+	CONSTRUCTIONS: {
+		$con_data = XMLin("../databases/con_db.xml", ForceArray => 1);	# readin the XML data, note that any hash with properties will recieve an array index even if there is only one of that hash
+		open (CON_DB_XML, '>', "../databases/con_db_regen.xml") or die ("can't open  ../databases/con_db_regen.xml");	# open a writeout file
+		print CON_DB_XML XMLout($con_data);	# printout the XML data
+		close CON_DB_XML;
+
+		open (CON_DB, '>', "../databases/con_db_xml.a") or die ("can't open  ../databases/con_db_xml.a");	# open a writeout file for the constructions
+		open (CON_LIST, '>', "../databases/con_db_xml_list") or die ("can't open  ../databases/con_db_xml_list");	# open a list file that will simply list the materials 
+
+		print CON_DB "# composite constructions database (columnar format) constructed from con_db.xml by DB_Gen.pl based on mat_db.xml\n#\n";	# heading intro line
+		print CON_LIST "# composite constructions database (columnar format) constructed from con_db.xml by DB_Gen.pl based on mat_db.xml\n#\n";	# heading intro line
+
+		printf CON_DB ("%5d%s\n", $#{$con_data->{'construction'}} + 1," # total number of constructions\n#");	# print the number of constructions
+
+		printf CON_DB ("%s\n%s\n%s\n",	# format instructions for the construction database
+			"# for each construction list the: # of layers, construction name, type (OPAQ or TRAN), Optics name (or OPAQUE), symmetry.",
+			"#\t followed by for each material of the construction:",
+			"#\t\t material number, thickness (m), material name, and if 'Gap' then RSI at vert horiz and sloped"
+		);
+
+		foreach my $con (@{$con_data->{'construction'}}) {	# iterate over each construction
+			print CON_DB "#\n#\n# CONSTRUCTION\n";	# print a common identifier
+
+			print CON_LIST "\n$con->{'con_name'} : $con->{'type'} : $con->{'symmetry'} : $con->{'description'}\n";	# print the construction name and description to the list
+
+			printf CON_DB ("%5d    %-14s%-6s", 	# print the construction information
+				$#{$con->{'layer'}} + 1,	# number of layers in the construction
+				$con->{'con_name'},	# construction name
+				$con->{'type'}	# type of construction (OPAQ or TRAN)
+			);
+			$con_name->{$con->{'con_name'}} = $con;
+
+			if ($con->{'type'} eq "OPAQ") {printf CON_DB ("%-14s", "OPAQUE");}	# opaque so no line to optics database
+			elsif ($con->{'type'} eq "TRAN") {printf CON_DB ("%-14s", $con->{'optic_name'});};	# transluscent construction so link to the optics database
+
+
+			printf CON_DB ("%-14s\n", $con->{'symmetry'});	# print symetrical or not
+
+			print CON_DB "# $con->{'description'}\n";	# print the construction description
+			print CON_DB "#\n# MATERIALS\n";	# print a common identifier
+
+			foreach my $layer (@{$con->{'layer'}}) {	# iterate over construction layers
+				# check if the material is Gap
+				if ($layer->{'mat_name'} eq 'gap') {	# check spelling of Gap and fix if necessary
+					$layer->{'mat_name'} = "Gap";
+				};
+
+				printf CON_DB ("%5d%10.4f",	# print the layers number and name
+					$mat_name->{$layer->{'mat_name'}}->{'mat_num'},	# material number
+					$layer->{'thickness_mm'} / 1000	# material thickness in (m)
+				);
+
+				if ($layer->{'mat_name'} eq 'Gap') {	# it is Gap based on material number zero
+					# print the RSI properties of Gap for the three positions that the construction may be placed in
+					printf CON_DB ("%s%4.3f %4.3f %4.3f\n",
+						"  Gap  ",
+						$layer->{'gap_RSI'}->[0]->{'vert'},
+						$layer->{'gap_RSI'}->[0]->{'horiz'},
+						$layer->{'gap_RSI'}->[0]->{'slope'}
+					);
+				}
+				else {	# not Gap so simply report the name and descriptions
+					print CON_DB "  $layer->{'mat_name'} : $mat_name->{$layer->{'mat_name'}}->{'description'}\n";	# material name and description from the list
+				};
+
+				print CON_LIST "\t$layer->{'mat_name'} : $layer->{'thickness_mm'} (mm) : $mat_name->{$layer->{'mat_name'}}->{'description'}\n";	# material name and description
+			};
+		};
+		close CON_DB;
+		close CON_LIST;
+	};
+
+	return ($mat_name, $con_name, $optic_data);
+};
+
+
+# Final return value of one to indicate that the perl module is successful
+1;
