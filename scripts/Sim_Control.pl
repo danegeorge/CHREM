5b6f3e02139532bf916f112d0c9e829b834e1bb5
lswan <lswan@f32d5ad9-0110-4c90-bc82-9769d9c816da>
2010-01-22 19:50:39 +0000

    Summary: This commit unfortunately rolls back previous efforts to get the simulation into one file.
    Although my previous attempt to mult-thread with a changing of directories was successful, it had the result 
    of limiting the simulation computer to using 1 of 8 cores. This obviously is very slow.
    
    This commit rolls back to the two script model which can again take advantage of all simulating cores.
    It is similar to r350, but has the benefits of specifying particular house names and running ish on all zones 
    for insolation purposes.
    
    
    
    Former-commit-id: a1971202529e9b27d20cc80dc06a66a98fd385f0

--- scripts/Sim_Control.pl
+++ scripts/Sim_Control.pl
@@ -16,17 +16,15 @@
 # and then intiates the simulations. The script reads the directories based on 
 # the house type (SD or DR) and # region (AT, QC, OT, PR, BC). Which types and 
 # regions are generated is specified at the beginning of the script to allow for 
-# partial generation. Note that the beginning of a house folder name may be specified
-# to limit the simulation to only matching houses
+# partial generation.
 # 
 # The script adds the list of houses to an array and then divides the array by the
 # total number of CPU cores used for simulation. It then writes text files with 
-# the paths for each house that the simulator will follow and then simulate,
-# as well as prescribes these houses to each cores variables.
+# the paths for each house that the simulator will follow and then simulate.
 # 
-# The script then performs a system call to conduct the simulations and 
-# stores the output information. This information is returned and merged into
-# a large hash ref.
+# The script then calls another simulating script using the nohup command to account
+# for chdir() and background simulation requirements as detailed below. This script 
+# quickly ends, leaving the appropriate simulating scripts running.
 
 
 #===================================================================
@@ -40,61 +38,31 @@ use strict;
 #use CSV;		#CSV-2 (for CSV split and join, this works best)
 #use Array::Compare;	#Array-Compare-1.15
 #use Switch;
-use threads;		#threads-1.71 (to multithread the program)
-# use threads::shared;
+#use threads;		#threads-1.71 (to multithread the program)
 #use File::Path;	#File-Path-2.04 (to create directory trees)
 #use File::Copy;	#(to copy the input.xml file)
 use Data::Dumper;
 
-use Hash::Merge qw(merge); # to support merging the results of different house types and regions
-
-# CHREM modules
 use lib ('./modules');
 use General;
-
-$Data::Dumper::Sortkeys = \&order;
-
-Hash::Merge::specify_behavior(
-	{
-		'SCALAR' => {
-			'SCALAR' => sub {$_[0] + $_[1]},
-			'ARRAY'  => sub {[$_[0], @{$_[1]}]},
-			'HASH'   => sub {$_[1]->{$_[0]} = undef},
-		},
-		'ARRAY' => {
-			'SCALAR' => sub {[@{$_[0]}, $_[1]]},
-			'ARRAY'  => sub {[@{$_[0]}, @{$_[1]}]},
-			'HASH'   => sub {[@{$_[0]}, $_[1]]},
-		},
-		'HASH' => {
-			'SCALAR' => sub {$_[0]->{$_[1]} = undef},
-			'ARRAY'  => sub {[@{$_[1]}, $_[0]]},
-			'HASH'   => sub {Hash::Merge::_merge_hashes($_[0], $_[1])},
-		},
-	}, 
-	'Merge where scalars are added, and items are (pre)|(ap)pended to arrays', 
-);
-
 
 #--------------------------------------------------------------------
 # Declare the global variables
 #--------------------------------------------------------------------
 my $hse_types;	# declare an hash array to store the house types to be modeled (e.g. 1 -> 1-SD)
 my $regions;	# declare an hash array to store the regions to be modeled (e.g. 1 -> 1-AT)
-my $cores;	# store the input core info
-my @houses_desired; # declare an array to store the house names or part of to look
-
+my $cores;
 #--------------------------------------------------------------------
 # Read the command line input arguments
 #--------------------------------------------------------------------
 COMMAND_LINE: {
-	if (@ARGV < 3) {die "A minimum Three arguments are required: house_types regions core_information [house names]\n";};
+	if ($#ARGV != 2) {die "Three arguments are required: house_types regions core_information\n";};
 	
 	# Pass the input arguments of desired house types and regions to setup the $hse_types and $regions hash references
-	($hse_types, $regions) = &hse_types_and_regions(shift (@ARGV), shift (@ARGV));
+	($hse_types, $regions) = hse_types_and_regions(@ARGV[0..1]);
 
 	# Check the cores arguement which should be three numeric values seperated by a forward-slash
-	unless (shift(@ARGV) =~ /^([1-9]?[0-9])\/([1-9]?[0-9])\/([1-9]?[0-9])$/) {
+	unless ($ARGV[2] =~ /^(\d+)\/(\d+)\/(\d+)$/) {
 		die ("CORE argument requires three Positive numeric values seperated by a \"/\": #_of_cores/low_core_#/high_core_#\n");
 	};
 	
@@ -115,240 +83,65 @@ COMMAND_LINE: {
 		die ("CORE argument numeric values are inappropriate (e.g. high_core > #_of_cores)\n");
 	};
 	
-	# Store an all cores array to divide up the directories
-	$cores->{'all'} = [1..$cores->{'num'}];
-	
-	# Store a simulation array so we only go to desired cores for simulation
-	$cores->{'sim'} = [$cores->{'low'}..$cores->{'high'}];
-
-	# Provide support to only simulate some houses
-	@houses_desired = @ARGV;
-	# In case no houses were provided, match everything
-	if (@houses_desired == 0) {@houses_desired = '.'};
-	
 };
-# print Dumper $cores;
 
 #--------------------------------------------------------------------
 # Identify the house folders for simulation
 #--------------------------------------------------------------------
-my @dirs;	#declare an array to store the path to each hse which will be simulated
+my @folders;	#declare an array to store the path to each hse which will be simulated
 
-foreach my $hse_type (&array_order(values %{$hse_types})) {		#each house type
-	foreach my $region (&array_order(values %{$regions})) {		#each region
-		push (my @folders, <../$hse_type/$region/*>);	#read all hse directories and store them in the array
-# 		print Dumper @folders;
-		CHECK_FOLDER: foreach my $folder (@folders) {
-			# cycle through the desired house names to see if this house matches. If so continue the house build
-			foreach my $desired (@houses_desired) {
-				# it matches, so set the flag
-				if ($folder =~ /\/$desired/) {
-					push (@dirs, $folder);
-					next CHECK_FOLDER;
-				};
-			};
-		};
+foreach my $hse_type (sort {$a cmp $b} values (%{$hse_types})) {		#each house type
+	foreach my $region (sort {$a cmp $b} values (%{$regions})) {		#each region
+	push (@folders, <../$hse_type/$region/*>);	#read all hse directories and store them in the array
 	};
 };
 
-# print Dumper @dirs;
+# print Dumper @folders;
 
 #--------------------------------------------------------------------
 # Determine how many houses go to each core for core usage balancing
 #--------------------------------------------------------------------
-my $interval = int(@dirs/$cores->{'num'}) + 1;	#round up to the nearest integer
-# print Dumper $interval;
-my $core_dirs = {};	# store the directories for later lookup during simulation
+my $interval = int(@folders/$cores->{'num'}) + 1;	#round up to the nearest integer
+
 
 #--------------------------------------------------------------------
 # Generate and print lists of directory paths for each core to simulate
 #--------------------------------------------------------------------
 SIMULATION_LIST: {
-	# only print out one file now as it is simply for reporting
-	my $file = "../summary_files/hse_listing_per_core";
-	my $ext = '.csv';
-	open (my $FILE, '>', $file . $ext) or die ("can't open $file$ext");	#open the file to print the list for the core
-	
-	# Cycle over all the cores
-	foreach my $core (@{$cores->{'all'}}) {
-		print $FILE "CORE $core\n"; # reporting
-		
-		# initialize here so that later we can loop on this even if no houses are specified
-		$core_dirs->{$core} = [];
-		
-		# Typical path to get the first interval many houses
-		if (@dirs > 0 && $interval <= @dirs) {
-			# Cycle over the houses and store them
-			foreach (1..$interval) {
-				my $folder = shift(@dirs);
-				push (@{$core_dirs->{$core}}, $folder);
-				print $FILE "\t" . $folder . "\n";	#print the hse path to the list
-			};
+	foreach my $core (1..$cores->{'num'}) {
+		my $low_element = ($core - 1) * $interval;	#hse to start this particular core at
+		my $high_element = $core * $interval - 1;	#hse to end this particular core at
+		if ($core == $cores->{'num'}) { $high_element = $#folders};	#if the final core then adjust to end of array to account for rounding process
+		open (HSE_LIST, '>', "../summary_files/hse_list_core_$core.csv") or die ("can't open ../summary_files/hse_list_core_$core.csv");	#open the file to print the list for the core
+		foreach my $element ($low_element..$high_element) {
+			print HSE_LIST "$folders[$element]\n";	#print the hse path to the list
 		}
-		# This is for when there are houses left but not as many as the interval
-		elsif (@dirs > 0) {
-			foreach (0..$#dirs) {
-				my $folder = shift(@dirs);
-				push (@{$core_dirs->{$core}}, $folder);
-				print $FILE "\t" . $folder . "\n";	#print the hse path to the list
-			};
-		}
-		# This is for when we are empty of houses
-		else {
-			print $FILE "\n";	#print the hse path to the list
-		};
-		
-		print $FILE "\n\n";
+		close HSE_LIST;		#close the particular core list
 	};
 };
 
-# print Dumper $core_dirs
 
 
 #--------------------------------------------------------------------
 # Call the simulations.
 #--------------------------------------------------------------------
 SIMULATION: {
+	# This is done using the nohup command for two reasons:
+	# 1) chdir() cannot be used with multi-threading as they all have the same 
+	# current working directory. "bps" must be run in the directory to eliminate issues
+	# with the res file looking in the wrong director and the xml files being generated 
+	# in the top level directory
+	# 2) nohup allows for the shutdown of the remote terminal and the continuation of the
+	# simulation. This is very important as the simulation may take a long time. It also 
+	# records all the screen output of the simulations. Note - as the simulations have 
+	# been released as their own, to kill them requires that each (# of cores) perl script 
+	# (Sim_Core_V1.pl) is killed and then either let the bps finish that house or kill it 
+	# as well
+	#--------------------------------------------------------------------
 
-	my $thread = {}; # declare threads
-
-	my $summary = {}; # declare a summary storage
-
-	# Multithread
-	foreach my $core (@{$cores->{'sim'}}) {
-		$thread->{$core} = threads->new(\&main, $core, $core_dirs);	# Spawn the threads and send to main subroutine
-	};
-
-	# Return the multithread
-	foreach my $core (@{$cores->{'sim'}}) {
-		$thread->{$core} = $thread->{$core}->join();	# Return the threads together for info collation
-# 		print Dumper $thread->{$core};
-
-		# Merge in the summary results to the total hash
-		$summary = merge($summary, $thread->{$core});
-	};
-	
-# 	print Dumper $summary;
-	my $file = '../summary_files/Combined_Results';
-	my $ext = '.csv';
-
-	open (my $FILE, '>', $file . $ext) or die ("can't open $file$ext");	#open the file to print the list for the core
-	print $FILE Dumper $summary;
-# 	foreach my $hse_type (&order(values %{$hse_types})) {		#each house type
-# 		foreach my $region (&array_order(values %{$regions})) {		#each region
-
-};
-
-
-SUBROUTINES: {
-# MAIN subroutine to do the simulations
-	sub main {
-		# determine the passed variables
-		my $core = shift;
-		my $core_dirs = shift;
-		
-# 		print Dumper $core_dirs;
-		
-		# The log file with reference to folder 'scripts'
-		my $log = "../summary_files/sim_output_core_$core.txt";
-		# Delete the file
-		unlink $log;
-		
-		# Relocate the log relative to the house folders
-		$log = '../../' . $log;
-		
-		# Local summary storage variables
-		my $summary = {};
-		
-		my $counter = 0;
-		my $total = @{$core_dirs->{$core}};
-		
-		# Cycle over the house directories
-		foreach my $dir (@{$core_dirs->{$core}}) {
-			# Determine the type, region, and house
-			my ($hse_type, $region, $house) = ($dir =~ /(\w+-\w+)\/(\w+-\w+)\/(\w+)$/);
-			
-			# Shorten the name using a ref
-			my $summ = \%{$summary->{$hse_type}->{$region}};
-			
-			# Append the cfg to the house for the filename to simulate
-			my $file = $house . '.cfg';
-			
-			open (my $FILE, '<', "$dir/$file") or die ("can't open $dir/$file");	#open the file to read
-			
-			my @ish;
-			
-			while (<$FILE>) {
-				if ($_ =~ /^\*isi \.\/\w+\.(\w+)\.shd$/) {
-					push (@ish, $1);
-				};
-			};
-			close $FILE;
-			
-# 			print Dumper @ish;
-			
-			# System call - NOTE: The 'cd' is within the system call as multithreading does not support different working directories (global variable). If one thread does a chdir then it would also affect other threads
-			
-			foreach my $zone (@ish) {
-# 				system "cd $dir\nish -mode text -file $file -zone $zone -act update_silent >> $log";
-				system "cd $dir\nish -mode text -file $file -zone $zone -act update_silent";
-			};
-			
-# 			system "cd $dir\nbps -mode text -file $file -p sim_presets silent >> $log";
-			system "cd $dir\nbps -mode text -file $file -p sim_presets silent";
-			
-			$counter++;
-			my $percent = sprintf("%.0f%s", $counter/$total * 100, '%');
-			print "Core $core: Completed sim $counter/$total ($percent)\n";
-			
-			# locate the summary file
-			$file = $dir . '/out';
-			my $ext = '.summary';
-			
-			# If a summary file exists
-			if (-e $file . $ext) {
-				# Open the summary file for readin
-				open (my $FILE, '<', $file . $ext) or die ("can't open $file$ext");	#open the file to print the list for the core
-				
-				# cycle over the lines and store the info
-				while (<$FILE>) {
-					# the trim
-					my $line = rm_EOL_and_trim($_);
-					
-					# Split into field and result based on the double colon (xml output style)
-					my ($field, $result) = split(/::/, $line);
-					
-					# Determine the type (e.g. min, max, annual), the value and units
-					my ($type, $value, $unit) = split(/\s+/, $result);
-					
-					# Strip the surrounding paranthesis off the units
-					$unit =~ s/^\((.+)\)$/$1/;
-					
-					# Delcare a unit format and check to see if it is defined, otherwise consider it a string
-					my $format;
-					$format = {'GJ' => "%.1f", 'W' => "%.0f", 'oC' => "%.1f", '%' => "%.1f", 'K' => "%.1f", , 'cm' => "%.1f"}->{$unit} or $format = "%s";
-					$value = sprintf($format, $value);
-					
-					# Store these things in the summary
-					@{$summ->{$field}->{$house}->{$type}}{qw(value unit)} = ($value, $unit);
-					
-					$summary->{'fields'}->{$field} = 0;
-					$summary->{'type'}->{$type} = 0;
-					
-					
-				};
-			}
-			
-			# If the out.summary does not exist, then record this
-			else {
-				$summary->{'no-summary'} = $house;
-			};
-		};
-# 	print Dumper $summary;
-	# Return the summary
-	return ($summary);
-	};
-
-	print "THE HOUSE LISTINGS FOR EACH CORE TO SIMULATE ARE LOCATED IN ../summary_files/hse_listing_per_core.csv\n";
+	foreach my $core ($cores->{'low'}..$cores->{'high'}) {	#simulate the appropriate list (i.e. QC2 goes from 9 to 16)
+		system ("nohup ./Core_Sim.pl $core > ../summary_files/sim_output_core_$core.txt &");	#call nohup of simulation program script and pass the argument $core so the program knows which set to simulate
+	} 
+	print "THE HOUSE LISTINGS FOR EACH CORE TO SIMULATE ARE LOCATED IN ../summary_files/hse_list_core_X.csv\n";
 	print "THE HOUSE SIMULATION OUTPUT FROM EACH CORE IS LOCATED IN ../summary_files/sim_output_core_X.txt\n";
 };
