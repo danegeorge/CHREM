2d7885f11ee501fa8aeee3c1747f85e46e85359f
lswan <lswan@f32d5ad9-0110-4c90-bc82-9769d9c816da>
2010-03-29 20:29:39 +0000

    Summary: A major energy balance summary addition to Core_Sim.pl
    It was found that there was great difficulty in getting the ESP-r results to balance. A TON of effort has gone into this and it is now working.
    This commit provides the support to analyze the house.xml log file (XML format) and display the integrated fluxes in such a way as to determine energy balance.
    The description of the balance will be left to the ESP-r commit (coming shortly), but in general terms shows the following:
    
    An energy balance in created to surround the zone air point and the interior surface nodes.
    The conduction, LW radiation (including to surfaces, plant, and casual gains), and SW radiation are summed for each surface by surface type (transparent or opaque).
    The infiltration, ambient-zone mechanical ventilation, zone-coupled heat advection, heating system, cooling system, and casual gain convection is summed.
    The two preceding items are found to be equal to the stored energy change in the air point, opaque, and transparent node masses.
    
    The results print out in an house.energy_balance file in a vertically organized pattern for easy readibility.
    
    
    
    
    Former-commit-id: a76ed4a500d77e16b723d40ac296d3f21d748bca

--- scripts/Core_Sim.pl
+++ scripts/Core_Sim.pl
@@ -29,6 +29,8 @@ use CSV;		#CSV-2 (for CSV split and join, this works best)
 #use File::Path;	#File-Path-2.04 (to create directory trees)
 use Cwd;		#(to determine current working directory)
 use Data::Dumper;
+use XML::Simple;	# to parse the XML
+use Storable  qw(dclone);
 
 use lib ('./modules');
 use General;
@@ -79,7 +81,7 @@ SIMULATION: {
 
 	# The HOUSE LOOP for simulation
 	HOUSE: foreach  my $folder (@folders) { # Do until the house list is exhausted
-		print $FILE '*data,'; # Start storage of the simulation status for this house
+		print $FILE "*data,"; # Start storage of the simulation status for this house
 		$house_count++; # Increment the house counter
 		
 		# Folder information
@@ -88,7 +90,9 @@ SIMULATION: {
 		chdir ($folder); # Change to the appropriate directory for simulation. Sim has to be in directory for xml output
 
 		# House name and CFG file to determine ish zones
-		my ($house_name) = ($folder =~ /^.+(\w{10})(\/$|$)/); # Determine the house name which is the last 10 digits (note that a check is done for an extra slash)
+		my ($hse_type, $region, $house_name) = ($folder =~ /^\.\.\/(.+)\/(.+)\/(\w+)(\/$|$)/); # Determine the house name which is the last 10 digits (note that a check is done for an extra slash)
+		my $coordinates = {'hse_type' => $hse_type, 'region' => $region, 'file_name' => $house_name};
+		
 		my $cfg = "./$house_name.cfg";
 
 		
@@ -97,11 +101,14 @@ SIMULATION: {
 		unlink "./$house_name.ish"; # Unlink (delete) the previous ish file that held any ish output
 
 		open (my $CFG, '<', $cfg) or die ("\n\nERROR: can't open $cfg\n"); # Open the cfg file to check for isi
+		my @cfg;
+		while (<$CFG>) {
+			push(@cfg,&rm_EOL_and_trim($_));
+		};
+		close $CFG; # We are done with the CFG file
 		
 		# Cycle over the CFG file using the grep command and look for *isi tags - when one is found, store the zone name
-		my @isi_zones = grep (s/^\*isi \.\/\w+\.(\w+)\.shd$/$1/, &rm_EOL_and_trim(<$CFG>));
-		
-		close $CFG; # We are done with the CFG file
+		my @isi_zones = grep (s/^\*isi \.\/\w+\.(bsmt|main_\d)\.shd$/$1/, @cfg);
 		
 		# Cycle over the isi zones and do the ish shading analysis on that zone
 		foreach my $isi_zone (@isi_zones) {
@@ -156,7 +163,7 @@ SIMULATION: {
 			foreach my $key (@{&order($warnings)}) { # Cycle over the warnings
 				print $FILE ":'$key'=" . @{$warnings->{$key}}; # Print out the start of the warning and the number of times it was encountered
 			};
-			print $FILE ',';
+			print $FILE ",";
 		}; # Denote that bps has errors
 
 		# Rename the XML reporting files with the house name. If this is true then it may be treated as a proxy for a successful simulation
@@ -182,58 +189,204 @@ SIMULATION: {
 			next HOUSE; # Jump to the next house
 		}
 
-
-		my $file = "./$house_name.summary";
+		# Examine the cfg file and create a key of zone numbers to zone names
+		my @zones = grep (s/^\*geo \.\/\w+\.(\w+)\.geo$/$1/, @cfg); # Find all *.geo files and filter the zone name from it
+		my $zone_name; # Intialize a storage of zone name value at zone number key
+		foreach my $element (0..$#zones) { # Cycle over the array of zones by element number so it can be used
+			$zone_name->{$element + 1} = $zones[$element]; # key = index + 1, value is zone name
+		};
 		
-		if (open (SUMMARY, '<', $file)) {     #open the summary file to reorder it
-			my $results;
-			while (<SUMMARY>) {
-			# Lukas/zone_01/active_cool::Total_Average -311.102339 (W)
-			# Lukas/MCOM::Minimum 28.000000 (#)
-			#       if ($_ =~ /(.*)::(\w*)\s*(\w*\.\w*)\s*(\(.*\))/) {
-				my @split = split (/::|\s/, $_);
-				$results->{$split[0]}->{$split[1]} = [$split[2], $split[3]];
-			# 	};
+# 		print Dumper $zone_name;
+		
+		# Examine the cfg file and find the line containing the simulation presets, store the start month number so that it may be used as a key for the xml month reporting.
+		my @month = grep (s/^(\d{1,2}) (\d{1,2}) (\d{1,2}) (\d{1,2}) sim_presets$/$2/, @cfg);
+
+		# Read in the xml log file
+		my $file = "./$house_name.xml";
+		my $summary = XMLin($file);
+		
+		# Remove the 'parameter' field
+		$summary = $summary->{'parameter'};
+		
+# 		print Dumper $summary;
+		
+		# Create a month index hash which uses the month index as keys and the month name as values
+		my $index_month;
+		@{$index_month}{1..12} = qw(Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec); # Hash slice
+		
+		# Cycle over the entire summary xml file and reorder it for ease of access
+		foreach my $key (keys %{$summary}) {
+			# Strip the units value of brackets and store it as 'normal' units
+			my ($unit) = ($summary->{$key}->{'units'} =~ /\((.+)\)/);
+			$summary->{$key}->{'units'} = {'normal' => $unit};
+			
+			# Cycle over the binned_data (by month and annual) and relocate the data up the tree
+			foreach my $element (@{$summary->{$key}->{'binned_data'}}) {
+				my $period; # Define a period variable
+				if ($element->{'type'} eq 'annual') { # If the type is annual
+					$period = 'Period'; # Store the period
+					delete $element->{'type'}; # Delete the redundant information
+				}
+				elsif ($element->{'type'} eq 'monthly') { # Elsif the type is monthly
+					$period = $index_month->{$element->{'index'} + $month[0]}; # Store the period by month index, but add in the start month index as it may not be January
+					delete @{$element}{'type', 'index'}; # Delete the redundant information
+				}
+				else { # Report if the type is unknown
+					&die_msg("Bad XML reporting binned data type in $file: should be 'annual' or 'monthly'", $element->{'type'}, $coordinates);
+				};
+				# Save the information up the tree by cloning the remainder of the element to that period
+				$summary->{$key}->{$period} = dclone($element);
 			};
-			close SUMMARY;
-	# 		print Dumper ($results);
+			# Delete the redundant information
+			delete $summary->{$key}->{'binned_data'};
+			
+			# Cycle over the integrated data
+			foreach my $element (@{$summary->{$key}->{'integrated_data'}->{'bin'}}) {
+				my $period; # Define a period variable
+				if ($element->{'type'} eq 'annual') { # If the type is annual
+					$period = 'Period'; # Store the period
+				}
+				elsif ($element->{'type'} eq 'monthly') { # Elsif the type is monthly
+					$period = $index_month->{$element->{'number'} + $month[0]}; # Store the period by month index, but add in the start month index as it may not be January
+				}
+				else { # Report if the type is unknown
+					&die_msg("Bad XML reporting integrated data bin type in $file: should be 'annual' or 'monthly'", $element->{'type'}, $coordinates);
+				};
+				# Save the information (integrated value) up the tree under a key of 'integrated'
+				$summary->{$key}->{$period}->{'integrated'} = $element->{'content'};
+			};
+			# Also store the integrated units type
+			($summary->{$key}->{'units'}->{'integrated'}) = $summary->{$key}->{'integrated_data'}->{'units'};
+			# Delete the redundant information
+			delete $summary->{$key}->{'integrated_data'};
+		};
+		
+# 		print Dumper $summary;
+		
+		# Create an energy results hash reference to store accumulated data
+		my $en_results;
+		# The data will be sorted into a columnar printout, so store the width of the first column based on its header
+		$en_results->{'columns'}->{'variable'} = length('Energy (kWh)');
+		
+		# Cycle over the entire summary hash and summarize the control volume energy results
+		foreach my $key (keys %{$summary}) {
+			# Only summarize for energy balance based on zone information and a power type
+			if ($key =~ /^CHREM\/zone_0(\d)\/Power\/(.+)$/) {
+				my $zone_name2 = $zone_name->{$1}; # Store the zone name
+				my $variable = $2; # Store the variable name
+				
+				# Check the length of the variable and if it is longer, set the column to that width
+				if (length($variable) > $en_results->{'columns'}->{'variable'}) {
+					$en_results->{'columns'}->{'variable'} = length($variable);
+				};
+				
+				# Check to see if a column has been generated for this zone. If not then set it equal to the zone name length + 2 for spacing
+				unless (defined($en_results->{'columns'}->{$zone_name2})) {
+					$en_results->{'columns'}->{$zone_name2} = length($zone_name2) + 2;
+				};
+				
+				# Declare a type for sorting the results. Usually, a 1st law energy balance is DeltaE = Q - W.
+				# Because the DeltaE is likely to be little, we will show in vertical columns Q, then DeltaE
+				my $type;
+				if ($variable =~ /^(SH|LH)/) {$type = 'storage';}
+				elsif ($variable =~ /Opaq/) {$type = 'opaque';}
+				elsif ($variable =~ /Tran/) {$type = 'transparent';}
+				else {$type = 'air point'};
+
+				# Store the resulting information. Convert from GJ to kWh and format so the sign is always shown
+				if ($summary->{$key}->{'units'}->{'integrated'} eq 'GJ') {
+					$en_results->{$type}->{$variable}->{$zone_name2} = sprintf("%+.0f", $summary->{$key}->{'Period'}->{'integrated'} * 277.78);
+				}
+				else {&die_msg("Bad integrated data units for energy balance: should be 'GJ'", $summary->{$key}->{'units'}->{'integrated'}, $coordinates);};
+
+				# NOTE: Because interior convection with reference to the node is opposite our control volume, it needs a sign reversal
+# 				if ($variable =~ /^CV/) {
+# 					$en_results->{$type}->{$variable}->{$zone_name2} = sprintf("%+.0f", -$en_results->{$type}->{$variable}->{$zone_name2});
+# 				};
+
+				# Compare the length of this value to the column size and modify if necessary
+				if (length($en_results->{$type}->{$variable}->{$zone_name2}) > $en_results->{'columns'}->{$zone_name2}) {
+					$en_results->{'columns'}->{$zone_name2} = length($en_results->{$type}->{$variable}->{$zone_name2}) + 2;
+				};
+			};
+		};
+		
+# 		print Dumper $en_results;
+		
+		# Create a results file
+		$file = "./$house_name.energy_balance";
+		open (my $PERIOD, '>', $file);
+		
+		# Print the first column name of the header row, using the width specifified and a format involving a vertical bar afterwards
+		printf $PERIOD ("%-$en_results->{'columns'}->{'variable'}s |", 'Energy (kWh)');
+		
+		# Select the printout orders
+		my $print->{'zones'} = &order($en_results->{'columns'}, [qw(main bsmt crawl attic roof)], ['']); # Only print desired zones
+		$print->{'type'} = [qw(opaque transparent), 'air point', qw(storage)]; # Print the following energy types
+		# The following three lines control the types of fluxes to be output
+		$print->{'opaque'} = &order($en_results->{'opaque'}, [qw(CD SW LW)], ['']); # CD CV SW LW
+		$print->{'transparent'} = &order($en_results->{'transparent'}, [qw(CD SW LW)], ['']); # CD CV SW LW
+		$print->{'air point'} = &order($en_results->{'air point'}, [qw(AV GN)], ['']); #AV GN
+		$print->{'storage'} = &order($en_results->{'storage'}, [qw(SH LH)], []); # SH LH
+		
+		# Print the zone names for each column using the width information and a double space afterwards
+		foreach my $zone (@{$print->{'zones'}}) {
+			printf $PERIOD ("%$en_results->{'columns'}->{$zone}s", $zone);
 		};
 
-		$file = "./$house_name.dictionary";
-		if (open (DICTIONARY, '<', $file)) {     #open the dictionary file to cross reference
-			my $results;
-			my $parameter;
-			while (<DICTIONARY>) {
-			# "Lukas/zone_01/active_cool","active cooling required by zone","(W)"
-				$_ =~ /"(.*)","(.*)","(.*)"/;
-				$parameter->{$1}->{'description'} = $2;
-				$parameter->{$1}->{'units'} = $3;
-			};
-			# print Dumper ($parameter);
-			close DICTIONARY;
-			
-			$file = "./$house_name.results";
-			open (RESULTS, '>', $file) or die ("can't open $file\n");     #open the a results file to write out the organized summary results
-			printf RESULTS ("%10s %10s %10s %10s %10s %10s %10s %-50s %-s\n", 'Integrated', 'Int units', 'Total Avg', 'Active avg', 'Min', 'Max', 'Units', 'Name', 'Description');
+		# Declare a sum so that we can print the summations and energy balance info
+		my $sum;
 
-			my @keys = sort keys (%{$results});  # sort results
-			my @values = ('AnnualTotal', 'Total_Average', 'Active_Average', 'Minimum', 'Maximum');
-			foreach my $key (@keys) {
-				foreach (@values) {unless (defined ($results->{$key}->{$_})) {$results->{$key}->{$_} = ['0', '-']};};
-				printf RESULTS ("%10.2f %10s %10.2f %10.2f %10.2f %10.2f %10s %-50s %-s\n",
-					$results->{$key}->{$values[0]}->[0],
-					$results->{$key}->{$values[0]}->[1],
-					$results->{$key}->{$values[1]}->[0],
-					$results->{$key}->{$values[2]}->[0],
-					$results->{$key}->{$values[3]}->[0],
-					$results->{$key}->{$values[4]}->[0],
-					$results->{$key}->{$values[4]}->[1],
-					$key,
-					$parameter->{$key}->{'description'});
+		# Cycle over the desired energy types
+		foreach my $type (@{$print->{'type'}}) {
+
+			# This is not expected to be tripped until the fluxes have been determined
+			# The following cycles back through the previous types (e.g. opaque, transparent, and air point) to sum them all up. It is expected that individually their sums will be non-zero, but together they will be close to zero and balance the upcoming storage values
+			if ($type eq 'storage') {
+				# Print header info
+				print $PERIOD ("\n\n--" . 'SUMMATION OF PREVIOUS FLUXES' . "--\n");
+				printf $PERIOD ("%-$en_results->{'columns'}->{'variable'}s |", '++SUM++');
+				# Cycle over the zones
+				foreach my $zone (@{$print->{'zones'}}) {
+					my $sum2 = 0; # Create a second summation
+					foreach my $type2 (keys(%{$sum->{$zone}})) { # Cycle over all the previously calculated types
+						$sum2 = sprintf("%+.0f", $sum2 + $sum->{$zone}->{$type2}); # Add them together
+					};
+					# Finally print out the formatted total within the column
+					printf $PERIOD ("%$en_results->{'columns'}->{$zone}s", $sum2);
+				};
 			};
-			close RESULTS;
 			
+			# Print a header line for indication of this flux type
+			print $PERIOD ("\n\n--" . uc($type) . "--\n");
+			
+			# Cycle over each matching  variable
+			foreach my $variable (@{$print->{$type}}) {
+				# Print the variable information
+				printf $PERIOD ("%-$en_results->{'columns'}->{'variable'}s |", $variable);
+				# Cycle over the zones
+				foreach my $zone (@{$print->{'zones'}}) {
+					# Initialize the summation of this energy flux type
+					unless (defined($sum->{$zone}->{$type})) {
+						$sum->{$zone}->{$type} = 0;
+					};
+					# Print the  formatted value
+					printf $PERIOD ("%$en_results->{'columns'}->{$zone}s", $en_results->{$type}->{$variable}->{$zone});
+					# Add it to the summation
+					$sum->{$zone}->{$type} = sprintf("%  +.0f", $sum->{$zone}->{$type} + $en_results->{$type}->{$variable}->{$zone});
+				};
+				print $PERIOD ("\n"); # Because we are columnar information we have multiple zones and when complete print an end of line
+			};
+			
+			# Print the sum for this type of energy flux (e.g. opaque)
+			printf $PERIOD ("%-$en_results->{'columns'}->{'variable'}s |", '++SUM++');
+			# Cycle over the zones and print the sum
+			foreach my $zone (@{$print->{'zones'}}) {
+				printf $PERIOD ("%$en_results->{'columns'}->{$zone}s", $sum->{$zone}->{$type});
+			};
 		};
+		# Close up the file
+		close $PERIOD;
 
 		# Print the simulation time for this house (seconds since 1970)
 		print $FILE time . "\n";
