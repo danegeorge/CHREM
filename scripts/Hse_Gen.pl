cdcf01aa8e9115162172c6acd2f1bd2615729df5
viormetti <viormetti@f32d5ad9-0110-4c90-bc82-9769d9c816da>
2011-09-29 20:32:36 +0000

    UPGRADE: THE FOLLOWING LOGIC WILL UPGRADE ALL WINDOWS TO TG LOW-E (320004)

--- scripts/Hse_Gen.pl
+++ scripts/Hse_Gen.pl
@@ -439,8 +439,9 @@ MAIN: {
 			
 
 # VITTORIO IORMETTI'S WORKSPACE TO UPGRADE AND RETROFIT HOUSES OF THE CSDDRD
+
 						# THE FOLLOWING LOGIC WILL UPGRADE ALL WINDOWS TO TG LOW-E
-# 						$record_indc->{'wndw'}->{$surface}->{'code'} = '323004';
+# 						$record_indc->{'wndw'}->{$surface}->{'code'} = '320004';
 
 
 # END OF VITTORIO'S WORKSPACE
@@ -1184,7 +1185,7 @@ MAIN: {
 							else {&die_msg ('GEO: Bad favourite window code', "WNDW_$1", $coordinates);};
 						};
 						# THE FOLLOWING LOGIC WILL UPGRADE ALL WINDOWS TO TG LOW-E
-						$record_indc->{'wndw'}->{$surface}->{'code'} = '323004';
+						$record_indc->{'wndw'}->{$surface}->{'code'} = '320004';
 
 						# THE FOLLOWING LOGIC WILL UPGRADE SG WINDOWS TO TG WINDOWS
 # 						$record_indc->{'wndw'}->{$surface}->{'code'} =~ /(\d)\d{2}(\d{3})/;
