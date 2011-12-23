7e7c4c260217abe4028011752b6b7c3aa5b4d935
snikoofard <snikoofard@f32d5ad9-0110-4c90-bc82-9769d9c816da>
2011-12-23 22:22:25 +0000

    Thsi commit modify two lines.

--- scripts/modules/Air_flow.pm
+++ scripts/modules/Air_flow.pm
@@ -51,7 +51,8 @@ sub afn_node {
 	$air_water = {'air' => 1, 'water' => 2}->{$air_water} || &die_msg('Bad AFN air_water type', $air_water, $coordinates);
 	
 	# Key the condition type to a number
-	$type = {'int_unk' => '0', 'bnd_wind_ind' => 3}->{$type} || &die_msg('Bad AFN node condition type', $type, $coordinates);
+	$type = {'int_unk' => '0', 'bnd_wind_ind' => 3}->{$type};
+	if ($type !~ /[0|3]/) {&die_msg('Bad AFN node condition type', $type, $coordinates);}
 
 	# Insert a node
 	&insert ($hse_file->{'afn'}, "#END_NODES", 1, 0, 0, "%s %u %u %.1f %.1f %u %.1f\n", $name, $air_water, $type, $height_m, $temperature_C, $data_1, $data_2);
@@ -230,7 +231,8 @@ sub afn_degrees {
 	# We have to get the 1 into the 5th element - so shift and push 3 times (i.e. 5, 4, 3, 2, 1, 8, 7, 6)
 	foreach (1..3) {push(@AFN_orientation,shift(@AFN_orientation));};
 	# We may not be on the front side - so determine how many more shift/pushes we need (Note that sides are at right angles and we have 8 directions, so go 2 each side change)
-	my $AFN_rotation = {'front' => 0, 'right' => 2, 'back' => 4, 'left' => 6}->{$surface} || &die_msg('Bad AFN surface - determining rotation index', $surface, $coordinates);
+	my $AFN_rotation = {'front' => 0, 'right' => 2, 'back' => 4, 'left' => 6}->{$surface};
+	if ($AFN_rotation !~ /[0|2|4|6]/) {&die_msg('Bad AFN surface - determining rotation index', $surface, $coordinates);}
 	# Now do the shift/push to account for the sides
 	foreach (1..$AFN_rotation) {push(@AFN_orientation,shift(@AFN_orientation));};
 
