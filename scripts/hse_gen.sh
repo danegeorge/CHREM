dbbdf2f0ebbfc9105bfa28fe884cde433bfccc11
snikoofard <snikoofard@f32d5ad9-0110-4c90-bc82-9769d9c816da>
2011-12-24 14:37:42 +0000

    modifyng hse_gen.sh.

--- scripts/hse_gen.sh
+++ scripts/hse_gen.sh
@@ -3,15 +3,15 @@
 # Run from current directory 
 #$ -cwd 
 # Name that appears in qstat 
-#$ -N Base_WAM_TMC 
+#$ -N WTM2010_CFC_30 
 # Memory allocated 
-#$ -l h_vmem=1G 
+#$ -l h_vmem=2G 
 # Run time for each simulation 
-#$ -l h_rt=00:30:00 
+#$ -l h_rt=02:00:00 
 # mail when job ends 
 #$ -m e 
 #$ -M s.nikoofard@dal.ca 
 # Run 1 times with SGE_TASK_ID going 
 # From 1 to 1, stepping by 1 
 #$ -t 1:1:1 
-perl /globalscartch/snikoofa/CHREM/Sara_Nikoofard/scripts/Core_Sim_clus.pl $SGE_TASK_ID _Base_WAM_TMC
+./Hse_Gen_sara_TOT.pl 2 1 WTM2010_CFC_30 10 1
